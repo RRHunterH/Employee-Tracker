f5b4da04911bb441c4990d5814c5435adbd6a8b3
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-02-27 04:32:20 -0500

    updated schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,9 +11,9 @@ INSERT INTO roles (title, salary, department_id) VALUES
 ('HR Manager', 65000.00, 3),
 ('HR Assistant', 45000.00, 3);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL),
-('Jane', 'Smith', 2, 1),
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Bob', 'Johnson', 3, 1),
 ('Mike', 'Johnson', 3, 1),
 ('Emily', 'Davis', 4, 3),
 ('Alex', 'Wilson', 5, 3),
